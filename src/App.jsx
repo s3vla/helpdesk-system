9e90fea362a1f1409cc90c17b54435a0083e679d
s3vla <ramalvmac@gmail.com>
2026-08-11 10:52:40 -0300

    chore: setup inicial do projeto Vite + React

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import LoginScreen from './components/LoginScreen'
+
+function App (){
+  return(
+    <div>
+     <LoginScreen/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/LoginScreen.jsx
@@ -0,0 +1,10 @@
+function LoginScreen(){
+    return(
+        <div>
+            <h1>Bem-vindo</h1>
+            <p>Acesse com seu email corporativo</p>
+        </div>
+    )
+}
+
+export default LoginScreen
